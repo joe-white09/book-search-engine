b9a8a7121c5c9f0abf6567189797c9d4e68fba4e
joe-white09 <joe_white09@yahoo.com>
2022-09-13 13:03:40 -0500

    uses graphql to addUser and login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,32 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        savedBooks: [Book]
+    }
 
+    type Book {
+        _id: ID
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        user(username: String!): User
+        books(username: String): [Book]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
 `;
 
 module.exports = typeDefs;
